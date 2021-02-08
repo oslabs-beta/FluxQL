70960938071029b8f5e1d68e154d19ff8f4835b7
Heidi Kim <heidiyoorakim@gmail.com>
2021-02-08 14:19:44 -0500

    created ContextObjs in contexts.jsx

---
+++ client/state/contexts.jsx
@@ -0,0 +1,7 @@
+import React, { createContext, useReducer } from 'react';
+
+export const GeneralContext = createContext();
+export const CodeContext = createContext();
+export const PSQLContext = createContext();
+export const MongoContext = createContext();
+export const AdviceContext = createContext();
