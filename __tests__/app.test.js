bd66c7accd38226a879f9a4ecafc1ab1f65214f5
Tommy Liang <tommyliangsays@gmail.com>
2021-02-16 16:22:00 -0500

    working on app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../client/App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
