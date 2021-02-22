e1f44d227cc5a9d699d5fcd71a76271760ac0f8e
Tommy Liang <tommyliangsays@gmail.com>
2021-02-22 15:22:10 -0500

    cleaning files, removed comments, export readme cleaned

--- client/containers/codeContainer.jsx
+++ client/containers/codeContainer.jsx
@@ -8,23 +8,30 @@ import { CodeContext } from '../state/contexts';
 const codeContainer = () => {
   const { codeState, codeDispatch } = useContext(CodeContext);
 
-  const displayCodeBtns = 
+  const displayCodeBtns = (
     <div className="codeButtons">
-      <div className="fileTab buttonClass" onClick={() => codeDispatch({type: 'SHOW_SCHEMA'})}>
+      <div
+        className="fileTab buttonClass"
+        onClick={() => codeDispatch({ type: 'SHOW_SCHEMA' })}
+      >
         <span className="noselect">schema.js</span>
         <div id="circle"></div>
       </div>
-      <div className="fileTab buttonClass" onClick={() => codeDispatch({type: 'SHOW_RESOLVER'})}>
+      <div
+        className="fileTab buttonClass"
+        onClick={() => codeDispatch({ type: 'SHOW_RESOLVER' })}
+      >
         <span className="noselect">resolver.js</span>
         <div id="circle"></div>
       </div>
       <ExportButton />
     </div>
+  );
 
   return (
     <div className="CodeContainer">
       {codeState.schema && displayCodeBtns}
-      <CodeBox/>
+      <CodeBox />
     </div>
   );
 };
