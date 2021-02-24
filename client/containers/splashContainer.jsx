fa223f6bf59f022c68fc00148c1668c35ed30b2a
Heidi Kim <heidiyoorakim@gmail.com>
2021-02-24 17:38:45 -0500

    added a link to our home logo to open up app

--- client/containers/splashContainer.jsx
+++ client/containers/splashContainer.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useGenContext } from '../state/contexts';
 
 export default function splashContainer() {
+  const { generalDispatch } = useGenContext();
+
   return (
     <>
       <div id='stars'></div>
       <div id='stars2'></div>
       <div id='stars3'></div>
-      <Link to='/' id='mainLink'>
-        <div id='homeTitle'>
+      <div id='homeTitle'>
+        <Link to='/app' id='mainLink' onClick={() =>{
+          setTimeout(generalDispatch({ type: 'OPEN_URI_MODAL' }), 1000);
+        }}>
           <img src='../assets/logoclear.png' width='300px' height='300px'></img>
+        </Link>
           <span>Dra<span id='QL'>QL</span>a</span>
-        </div>
-      </Link>
+      </div>
       <section id="scrollDown" className="pointerDown">
         <a id="scrollDownLink"href="#about"><span></span>scroll to learn more<br></br>or click on our logo to get started</a>
       </section>
